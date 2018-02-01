43ec28151aed9e6a736da86fa88f646f20894dd3
Keano Bellmore <keanobellmore@Interapts-MacBook-Pro.local>
2018-02-01 15:36:49 -0500

    macbook tracker

---
+++ Macbook-Tracker/Macbook-Tracker/macbook.swift
@@ -0,0 +1,20 @@
+//
+//  Class.swift
+//  Macbook-Tracker
+//
+//  Created by Keano Bellmore on 2/1/18.
+//  Copyright © 2018 Keano Bellmore. All rights reserved.
+//
+
+import Foundation
+
+class Macbook {
+    var color: String
+    var year: Int
+   
+    
+    init(color: String, year: Int) {
+        self.color = color
+        self.year = year
+       }
+}
--- Macbook-Tracker/Macbook-Tracker/main.swift
+++ Macbook-Tracker/Macbook-Tracker/main.swift
@@ -5,27 +5,33 @@
 import Foundation
 
 
+print("what color is your first Macbook?")
+var color = readLine()!
+
+print("what year is your first Macbook?")
+var year = Int(readLine()!)!
+
+var userMac = Macbook(color: color, year: year)
+
+//*****************************************************************
+print("what color is your second Macbook?")
+var color2 = readLine()!
+print("what year is your second Macbook?")
+var year2 = Int(readLine()!)!
+
+var userMac2 = Macbook(color: color2, year: year2)
 
 
-class Macbook {
-    var color = String()
-    var year = Int()
-    
-    init(color: String, year: Int) {
-        self.color = color
-        self.year = year
-    }
-}
+//*****************************************************************
+print("what color is your third Macbook?")
+var color3 = readLine()!
+print("what year is your third Macbook?")
+var year3 = Int(readLine()!)!
 
-print("what color is your Macbook?")
-var color = readLine()
+var userMac3 = Macbook(color: color3, year: year3)
+
+print("the color of your first macbook is \(userMac.color) and was made in \(userMac.year). the color of your second macbook is \(userMac2.color) and was made in \(userMac2.year). the color of your third macbook is \(userMac3.color) and was made in \(userMac3.year)")
 
 
-print("what year is your Macbook?")
-var year = Int(readLine()!)
 
-var userMac = Macbook(color: color!, year: year!)
-
-print("your Macbook color is \(userMac.color)")
-print("your Macbook year is \(userMac.year)")
 
